c691058cefdb38bd4687bd34526497b7b5b603ea
Ara-18 <ara.llancaya@gmail.com>
2026-08-10 00:08:44 -0500

    fix: corregir sintaxis de CORS_ORIGINS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,9 +18,7 @@ app = FastAPI(
 
 CORS_ORIGINS = os.getenv(
     "CORS_ORIGINS",
-    "http://localhost:5173,http://localhost:3000",
-     "http://localhost:5173,http://localhost:3000,https://quriy.vercel.app"
-
+    "http://localhost:5173,http://localhost:3000,https://quriy.vercel.app"
 ).split(",")
 
 app.add_middleware(
